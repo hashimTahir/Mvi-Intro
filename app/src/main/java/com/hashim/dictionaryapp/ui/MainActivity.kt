f24dc3f79e5ddc5e71f935ac1387625803c71a34
hashim <hashimtahir@live.com>
2021-01-30 03:09:33 +0500

    packages recreated.

--- app/src/main/java/com/hashim/dictionaryapp/ui/MainActivity.kt
+++ app/src/main/java/com/hashim/dictionaryapp/ui/MainActivity.kt
@@ -2,10 +2,11 @@
  * Copyright (c) 2021/  1/ 30.  Created by Hashim Tahir
  */
 
-package com.hashim.dictionaryapp
+package com.hashim.dictionaryapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hashim.dictionaryapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
