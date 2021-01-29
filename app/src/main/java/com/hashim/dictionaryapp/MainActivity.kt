0d2a6ce5b06957b32949e58ef1516d51d6da940b
hashim <hashimtahir@live.com>
2021-01-30 03:05:52 +0500

    Initial Commit

---
+++ app/src/main/java/com/hashim/dictionaryapp/MainActivity.kt
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2021/  1/ 30.  Created by Hashim Tahir
+ */
+
+package com.hashim.dictionaryapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
