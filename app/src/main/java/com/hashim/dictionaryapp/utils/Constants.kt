a0d07a9ed08726ba0e84b91ba09016d7f1e602f8
hashim <hashimtahir@live.com>
2021-01-31 02:49:23 +0500

    ApiKeys and Packages created.
    Signed-off-by: hashim <hashimtahir@live.com>

--- app/src/main/java/com/hashim/dictionaryapp/utils/Constants.kt
+++ app/src/main/java/com/hashim/dictionaryapp/utils/Constants.kt
@@ -8,5 +8,10 @@ class Constants {
     companion object {
 
         const val hTag = "hashimTimberTags %s"
+        const val H_API_KEY = "dict.1.1.20210130T213135Z.44bd8f4704becea0.9c8f66b489d9214ec17e51c36718e117de7c5166"
+        const val H_BASE_URL = "https://dictionary.yandex.net/api/v1/"
+//                "dicservice.json/getLangs?key=dict.1.1.20210130T213135Z.44bd8f4704becea0.9c8f66b489d9214ec17e51c36718e117de7c5166"
+
+//        https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=APIkey&lang=en-ru&text=time
     }
 }
