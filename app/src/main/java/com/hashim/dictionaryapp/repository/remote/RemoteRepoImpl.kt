03dffccb36101f3a16d8ca4c876825231af9341d
hashim <hashimtahir@live.com>
2021-03-13 20:55:36 +0500

    NetworkBoundResours wrapper added to handle loading and retrieval of data
    Signed-off-by: hashim <hashimtahir@live.com>

--- app/src/main/java/com/hashim/dictionaryapp/repository/remote/RemoteRepoImpl.kt
+++ app/src/main/java/com/hashim/dictionaryapp/repository/remote/RemoteRepoImpl.kt
@@ -1,11 +1,12 @@
 package com.hashim.dictionaryapp.repository.remote
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.Transformations
 import com.hashim.dictionaryapp.api.RetrofitService
+import com.hashim.dictionaryapp.repository.remote.responses.lookupresponse.SearchRes
 import com.hashim.dictionaryapp.ui.main.state.MainViewState
 import com.hashim.dictionaryapp.utils.DataState
 import com.hashim.dictionaryapp.utils.GenericApiRes
+import retrofit2.Response
 import javax.inject.Inject
 
 class RemoteRepoImpl @Inject constructor(
@@ -16,38 +17,23 @@ class RemoteRepoImpl @Inject constructor(
     override fun hSearchWord(query: String)
             : LiveData<DataState<MainViewState>> {
 
-        return Transformations.switchMap(
-            hRetrofitService.hSearchWord(
-                key = key,
-                wordLookup = query
-            )
-        ) { apiResponse ->
-            object : LiveData<DataState<MainViewState>>() {
-                override fun onActive() {
-                    super.onActive()
-                    when (apiResponse) {
-                        is GenericApiRes.ErrorRes -> {
-                            value = DataState.hError(
-                                apiResponse.errorMessage
-                            )
-                        }
-                        is GenericApiRes.SuccessRes -> {
-                            value = DataState.hData(
-                                message = null,
-                                data = MainViewState(
-                                    hSearchRes = apiResponse.body
-                                )
-                            )
-                        }
-                        is GenericApiRes.EmptyRes -> {
-                            value = DataState.hError(
-                                errorMessage = "Http 304"
-                            )
-                        }
-                    }
-                }
+        return object : NetworkBoundResource<SearchRes, MainViewState>() {
+            override fun hOnDataSuccess(response: GenericApiRes.SuccessRes<SearchRes>) {
+                hResult.value = DataState.hData(
+                    message = null,
+                    data = MainViewState(
+                        hSearchRes = response.body
+                    )
+                )
             }
 
-        }
+            override fun hCreateCall(): LiveData<GenericApiRes<SearchRes>> {
+                return hRetrofitService.hSearchWord(
+                    wordLookup = query,
+                    key = key
+                )
+            }
+
+        }.hToLiveData()
     }
 }
